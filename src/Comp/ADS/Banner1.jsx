aebb35b0ac49533cfb44f2af22c5d69a9e317fd0
iamravi99 <ravinder.explore@gmail.com>
2025-08-02 00:44:09 +0530

    mm

--- src/Comp/ADS/Banner1.jsx
+++ src/Comp/ADS/Banner1.jsx
@@ -2,25 +2,30 @@ import React, { useEffect } from 'react';
 
 const Banner1 = () => {
   useEffect(() => {
+    // Inject ad-provider.js script
     const script1 = document.createElement('script');
     script1.src = 'https://a.magsrv.com/ad-provider.js';
-    script1.type = 'application/javascript';
     script1.async = true;
+    script1.type = 'application/javascript';
     document.body.appendChild(script1);
 
-    const adPushScript = document.createElement('script');
-    adPushScript.type = 'application/javascript';
-    adPushScript.innerHTML = `(AdProvider = window.AdProvider || []).push({serve: {}});`;
-    document.body.appendChild(adPushScript);
+    // Push the ad once the script is loaded
+    const loadAd = () => {
+      if (window.AdProvider) {
+        window.AdProvider.push({ serve: {} });
+      }
+    };
+
+    script1.onload = loadAd;
 
     return () => {
+      // Clean up
       document.body.removeChild(script1);
-      document.body.removeChild(adPushScript);
     };
   }, []);
 
   return (
-    <div className="w-full flex justify-center my-4">
+    <div className="w-full flex justify-center my-6">
       <ins
         className="eas6a97888e2"
         data-zoneid="5691044"
