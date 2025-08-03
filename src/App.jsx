722f7d8df18686ff6221a0886d15bac9f71b70c3
iamravi99 <ravinder.explore@gmail.com>
2025-08-03 12:09:53 +0530

    Restore original

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import AgeCheckModal from './Comp/AgeCheckModal'; // ✅ use here
+import AgeCheckModal from './Comp/AgeCheckModal';
+import Layout from './Layout';
+
+// Public Components
 import Home from './Comp/Home/Home';
 import NewWeb from './Comp/NewWeb';
 import NewWebDetail from './Comp/NewWebDetail';
-import About from './Comp/About'; // Assuming you have an About component
-import Layout from './Layout';
+import About from './Comp/About';
 import UlluActresses from './Comp/UlluActresses';
 import UlluActressDetail from './Comp/UlluActressDetail';
 import Stories from './Comp/Stories';
@@ -15,6 +17,8 @@ import DesiLeaks from './Comp/DesiLeaks';
 import DesiLeakDetail from './Comp/DesiLeakDetail';
 import Viral from './Comp/Viral';
 import ViralDetail from './Comp/ViralDetail';
+
+// Admin Components
 import AdminDashboard from './admin/AdminDashboard';
 import AdminLogin from './admin/AdminLogin';
 import WebseriesManager from './admin/WebseriesManager';
@@ -22,7 +26,6 @@ import StoriesManager from './admin/StoriesManager';
 import UlluActressManager from './admin/UlluActressManager';
 import DesiLeaksManager from './admin/DesiLeaksManager';
 import ViralManager from './admin/ViralManager';
-
 
 function App() {
   const [isAllowed, setIsAllowed] = useState(false);
@@ -42,59 +45,39 @@ function App() {
   return (
     <BrowserRouter>
       {!isAllowed && <AgeCheckModal onAccept={() => setIsAllowed(true)} />}
+
       {isAllowed && (
         <Routes>
           <Route element={<Layout />}>
+
+            {/* Public Routes */}
             <Route path="/" element={<Home />} />
             <Route path="/newweb" element={<NewWeb />} />
             <Route path="/newweb/:id" element={<NewWebDetail />} />
-           <Route path="/about" element={<About/>} />
+            <Route path="/about" element={<About />} />
+            <Route path="/ulluactresses" element={<UlluActresses />} />
+            <Route path="/actress/:id" element={<UlluActressDetail />} />
+            <Route path="/stories" element={<Stories />} />
+            <Route path="/stories/:id" element={<StoryDetail />} />
+            <Route path="/desileaks" element={<DesiLeaks />} />
+            <Route path="/desileaks/:id" element={<DesiLeakDetail />} />
+            <Route path="/viral" element={<Viral />} />
+            <Route path="/viral/:id" element={<ViralDetail />} />
 
-
-<Route path="/ulluactresses" element={<UlluActresses />} />
-<Route path="/actress/:id" element={<UlluActressDetail />} />
-
-<Route path="/stories" element={<Stories />} />
-<Route path="/stories/:id" element={<StoryDetail />} />
-<Route path="/desileaks" element={<DesiLeaks />} />
-
-<Route path="/desileaks/:id" element={<DesiLeakDetail />} />
-
-
-
-
-
- <Route path="/viral" element={<Viral />} />
-          <Route path="/viral/:id" element={<ViralDetail />} />
-
-
-
-{/* Admin Routes */}
-<Route path="/singh/login" element={<AdminLogin onLogin={setIsAdminAuth} />} />
-{isAdminAuth ? (
-  <>
-    <Route path="/singh" element={<AdminDashboard />} />
-    <Route path="/singh/webseries" element={<WebseriesManager />} />
-    <Route path="/singh/stories" element={<StoriesManager />} />
-    <Route path="/singh/ulluactresses" element={<UlluActressManager />} />
-    <Route path="/singh/desileaks" element={<DesiLeaksManager />} />
-    <Route path="/singh/viral" element={<ViralManager />} />
-  </>
-) : (
-  <Route path="/singh/*" element={<AdminLogin onLogin={setIsAdminAuth} />} />
-)}
-
-
-
-
-
-
-
-
-
-
-
-
+            {/* Admin Routes */}
+            <Route path="/singh/login" element={<AdminLogin onLogin={setIsAdminAuth} />} />
+            {isAdminAuth ? (
+              <>
+                <Route path="/singh" element={<AdminDashboard />} />
+                <Route path="/singh/webseries" element={<WebseriesManager />} />
+                <Route path="/singh/stories" element={<StoriesManager />} />
+                <Route path="/singh/ulluactresses" element={<UlluActressManager />} />
+                <Route path="/singh/desileaks" element={<DesiLeaksManager />} />
+                <Route path="/singh/viral" element={<ViralManager />} />
+              </>
+            ) : (
+              <Route path="/singh/*" element={<AdminLogin onLogin={setIsAdminAuth} />} />
+            )}
 
           </Route>
         </Routes>
